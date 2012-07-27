32a6ccc948247c4e9dbd45d84bd054279f78979e
Michael Pirog <mike@eastbaywebshop.com>
2012-07-27 14:46:48 -0700

    remove about the author display

--- theme/templates/node--panopoly-news-article.tpl.php
+++ theme/templates/node--panopoly-news-article.tpl.php
@@ -87,7 +87,6 @@
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
   <div class="content"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
